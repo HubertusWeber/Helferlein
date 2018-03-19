42b6fe5fa7395efbe78084d47fc970f8b5bf71ae
Hubertus Weber <github@hubertus-weber.org>
2018-03-19 21:10:34 +0100

    Added cleanup for homebrew

--- bu.sh
+++ bu.sh
@@ -3,5 +3,16 @@ echo brew update
 brew update
 echo brew upgrade
 brew upgrade
+echo brew cleanup
+brew cleanup
 echo brew cask upgrade
 brew cask upgrade
+echo brew cask cleanup
+brew cask cleanup
+if [ $# -eq 1 ]
+  then
+  	echo updating tldr
+  	tldr --update
+  	echo updage/upgrade npm
+  	npm up
+fi
