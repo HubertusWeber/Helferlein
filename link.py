d9e5f31e77a2a141034c7f6c0ed5fc9c75581068
Hubertus Weber <github@hubertus-weber.org>
2018-04-08 04:24:47 +0200

    Added link.py to create Symlinks of all Helferlein in a specified Directory

---
+++ link.py
@@ -0,0 +1,22 @@
+#! /usr/bin/env python2
+
+#
+# This is a Meta-Helferlein, a Helferlein to help other Helferlein
+# It is here to link all the other Helferlein into a specified directory
+#
+
+import os
+import sys
+
+helferlein = {
+    "bu.py": "bu",
+    "bucket.py": "bucket",
+    "gg.py": "gg",
+    "hs.sh": "hs",
+    "s.sh": "s",
+}
+
+helferlein_path = os.path.dirname(os.path.realpath(__file__)) + "/"
+
+for src, dst in helferlein.iteritems():
+    os.system("ln -s " + helferlein_path + src + " " + sys.argv[1] + dst)
